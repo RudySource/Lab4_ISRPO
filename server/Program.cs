6898914e211dc1dfabaae203c91d8aab68477cca
Rud3us <rodionpylnev@yandex.ru>
2026-02-28 19:36:14 +0300

    Коммит 8 — добавление группы и даты

--- server/Program.cs
+++ server/Program.cs
@@ -1,17 +1,15 @@
-﻿namespace RudyLab4Server
-{
-    public class Program
-    {
-        static void Main(string[] args)
-    {
-          Console.WriteLine(@"
-            H   H  EEEEE  L      L       OOO  
-            H   H  E      L      L      O   O 
-            HHHHH  EEEE   L      L      O   O 
-            H   H  E      L      L      O   O 
-            H   H  EEEEE  LLLLL  LLLLL   OOO  
-            ");
-          Console.WriteLine($"ФИО: Rudy Rudy rudy");
-        }
-    }
+﻿namespace RudyLab4Server;
+public class Program{
+  static void Main(string[] args){
+    Console.WriteLine(@"
+      H   H  EEEEE  L      L       OOO  
+      H   H  E      L      L      O   O 
+      HHHHH  EEEE   L      L      O   O 
+      H   H  E      L      L      O   O 
+      H   H  EEEEE  LLLLL  LLLLL   OOO  
+      ");
+    Console.WriteLine($"ФИО: Rudy Rudy rudy");
+    Console.WriteLine($"Группа: ИСП-233");
+    Console.WriteLine($"Дата и время: {DateTime.Now}");
+  }
 }
