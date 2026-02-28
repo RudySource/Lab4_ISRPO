ed9d1b515244eba33bc3188af43d4bf7c483b6f6
Rud3us <rodionpylnev@yandex.ru>
2026-02-28 19:39:04 +0300

    Мне лень 9 и 10 разделять, поэтому сразу в одно...

--- server/Program.cs
+++ server/Program.cs
@@ -11,5 +11,25 @@ public class Program{
     Console.WriteLine($"ФИО: Rudy Rudy rudy");
     Console.WriteLine($"Группа: ИСП-233");
     Console.WriteLine($"Дата и время: {DateTime.Now}");
+    while (true){
+      Console.WriteLine("\nМеню:\n1 — Показать ФИО\n2 — Показать группу\n3 — Показать дату\n4 — Выход");
+      Console.Write("Выберите пункт меню: ");
+      switch (Console.ReadLine()){
+          case "1":
+              Console.WriteLine($"ФИО: Rudy Rudy rudy");
+              break;
+          case "2":
+              Console.WriteLine($"Группа: ИСП-233");
+              break;
+          case "3":
+              Console.WriteLine($"Дата и время: {DateTime.Now}");
+              break;
+          case "4":
+              return;
+          default:
+              Console.WriteLine("Неверный выбор.");
+              break;
+      }
+    }
   }
 }
